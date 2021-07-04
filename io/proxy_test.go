b69d5fff7896c7bd9530d12171a9063f9273bdbe
ulises <ulisescamps9@gmail.com>
2021-07-03 21:20:17 -0500

    a

--- io/proxy_test.go
+++ io/proxy_test.go
@@ -19,3 +19,19 @@ func proxy(from io.Reader, to io.Writer) error {
     return err
 }
 
+func TestProxy(t *testing.T) {
+    var wg sync.WaitGroup
+
+    // server listens for a "ping" message and responds with a "pong" message. All
+    // other messages are echoed back to the client
+    server, err := net.Listen("tcp", "127.0.0.1:")
+    if err != nil {
+        t.Fatal(err)
+    }
+    wg.Add(1)
+
+    go func() {
+        defer wg.Done()
+
+    }()
+}
