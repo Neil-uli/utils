547ce2a700971ba3366a8eead7cc307c42fb5691
ulises <ulisescamps9@gmail.com>
2021-07-30 23:54:00 -0500

    Data type implementation
    Signed-off-by: ulises <ulisescamps9@gmail.com>

--- udp/tftp/types.go
+++ udp/tftp/types.go
@@ -1,11 +1,11 @@
 package tftp
 
 import (
-	"bytes"
-	"encoding/binary"
-	"errors"
-	"io"
-	"strings"
+    "bytes"
+    "encoding/binary"
+    "errors"
+    "io"
+    "strings"
 )
 
 // TFTP limits datagram packets to 516 bytes or fewer to avoid fragmentation.
@@ -106,7 +106,7 @@ func (q *ReadReq) UnmarshalBinary(p []byte) error {
     if actual != "octet" {
         return errors.New("only binary transfers supported")
     }
-    return nil
+    return nil    
 }
 
 
@@ -118,14 +118,14 @@ type Data struct {
 func (d *Data) MarshalBinary() ([]byte, error) {
     b := new(bytes.Buffer)
     b.Grow(DatagramSize)
-    
+
     d.Block++
 
     err := binary.Write(b, binary.BigEndian, OpData)
     if err != nil {
         return nil, err
     }
-    
+
     err = binary.Write(b,binary.BigEndian, d.Block) 
     if err != nil { return nil, err }
 
@@ -135,4 +135,23 @@ func (d *Data) MarshalBinary() ([]byte, error) {
     }
 
     return b.Bytes(), nil
+}
+
+func (d *Data) UnmarshalBinary(p []byte) error {
+    if l := len(p); l < 4 || l > DatagramSize {
+        return errors.New("invalid DATA")
+    }
+
+    var opcode
+
+    err := binary.Read(bytes.NewReader(p[:2]), binary.BigEndian, &opcode)
+    if err != nil || opcode != OpData {
+        return errors.New("invalid DATA")
+    }
+    err = binary.Read(bytes.NewReader(p[2:4]), binary.BigEndian, &d.Block)
+    if err != nil {
+        return errors.New("invalid DATA")
+    }
+    d.Payload = bytes.NewBuffer(p[4:])
+    return nil
 }
