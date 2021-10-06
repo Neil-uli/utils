d16976d1b9e99be734441a14ee71a82f8fb59792
Neil Ulises <ulisescamps9@gmail.com>
2021-10-06 11:50:50 -0500

    instantiating a multiplexer and an HTTP server

--- http/server_test.go
+++ http/server_test.go
@@ -13,7 +13,22 @@ import (
 
 func TestSimpleHttpServer(t *testing.T) {
     srv := &http.Server{
+        Addr: "127.0.0.1:8080",
+        Handler: http.TimeoutHandler(
+            handlers.DefaultHandler(), 2*time.Minute, ""),
+        IdleTimeout: 5 * time.Minute,
+        ReadHeaderTimeout: time.Minute,
     }
+
+    l, err := net.Listen("tcp", srv.Addr)
+    if err != nil { t.Fatal(err) }
+
+    go func() {
+        err := srv.Serve(l)
+        if err != http.ErrServerClosed {
+            t.Error(err)
+        }
+    }()
 }
 
 
