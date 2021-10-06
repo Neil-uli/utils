846e1f864d2d290caa3147861264bbfe840a3ed5
Neil Ulises <ulisescamps9@gmail.com>
2021-10-06 14:29:55 -0500

    sending test requests to the server

--- http/server_test.go
+++ http/server_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"testing"
@@ -21,29 +22,58 @@ func TestSimpleHttpServer(t *testing.T) {
             ReadHeaderTimeout: time.Minute,
         }
 
-        l, err := net.Listen("tcp", srv.Addr)
-        if err != nil { t.Fatal(err) }
+    l, err := net.Listen("tcp", srv.Addr)
+    if err != nil { t.Fatal(err) }
 
-        go func() {
-            err := srv.Serve(l)
-            if err != http.ErrServerClosed {
-                t.Error(err)
-            }
-        }()
+    go func() {
+        err := srv.Serve(l)
+        if err != http.ErrServerClosed {
+            t.Error(err)
+        }
+    }()
 
-        testCases := []struct {
-            method   string
-            body     io.Reader
-            code     int
-            response string
-        }{
-            {http.MethodGet, nil, http.StatusOK, "Hello friend!"},
-            {http.MethodPost, bytes.NewBufferString("<world>"), http.StatusOK, "Hello, &lt;world&gt;!"},
-            {http.MethodHead, nil, http.StatusMethodNotAllowed, ""},
+    testCases := []struct {
+        method   string
+        body     io.Reader
+        code     int
+        response string
+    }{
+        {http.MethodGet, nil, http.StatusOK, "Hello, friend!"},
+        {http.MethodPost, bytes.NewBufferString("<world>"), http.StatusOK, "Hello, &lt;world&gt;!"},
+        {http.MethodHead, nil, http.StatusMethodNotAllowed, ""},
+    }
+
+    client := new(http.Client)
+    path := fmt.Sprintf("http://%s/", srv.Addr)
+
+    for i, c := range testCases {
+        r, err := http.NewRequest(c.method, path, c.body)
+        if err != nil {
+            t.Errorf("%d: %v", i, err)
+            continue
         }
 
-        client := new(http.Client)
-        path := fmt.Sprintf("http://%s/", srv.Addr)
-    }
+        resp, err := client.Do(r)
+        if err != nil {
+            t.Errorf("%d: %v", i, err)
+            continue
+        }
+
+        if resp.StatusCode != c.code {
+            t.Errorf("%d: unexpected status code: %q", i, resp.Status)
+        }
+
+        b, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            t.Errorf("%d: %v", i, err)
+            continue
+        }
+
+        _ = resp.Body.Close()
+        if c.response != string(b) {
+            t.Errorf("%d: expected %q; actual %q", i, c.response, b)
+        }
+    } 
+}
 
 
