0d2147755596d1ce256d16a0beab7ab2cb85154a
Neil Ulises <ulisescamps9@gmail.com>
2021-10-07 23:10:05 -0500

    a

--- udp/tftp/example/tftp.go
+++ udp/tftp/example/tftp.go
@@ -3,17 +3,25 @@ package main
 import (
 	"flag"
 	"io/ioutil"
-    "log"
-    "utils/udp/tftp"
+	"log"
+    "os"
+
+	"github.com/ulicod3/utils/udp/tftp"
 )
 
 var (
     address = flag.String("a", "127.0.0.1:69", "listen address")
     payload = flag.String("p", "payload.jpeg", "file to serve to clients")
+    help = flag.Bool("h, --help", false, "Show help")
 )
 
 func main() {
    flag.Parse()
+
+   if *help {
+        flag.PrintDefaults()
+        os.Exit(0) 
+   }
 
    p, err := ioutil.ReadFile(*payload)
    if err != nil { log.Fatal(err) }
